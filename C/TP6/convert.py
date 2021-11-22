791285cd101936e17649d43c1d73d89ef5677ec6
Sacane <johan.ramaroson@gmail.com>
2021-11-22 10:23:35 +0100

    (TP6 C) update method 'ToC'

--- C/TP6/convert.py
+++ C/TP6/convert.py
@@ -1,5 +1,8 @@
 from ctypes import *
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import math
 
 c_type = cdll.LoadLibrary("./image_process.so")
 
@@ -8,17 +11,20 @@ Pixel_c = c_ubyte * 4
 img_line = Pixel_c * size_pixel
 Img_C = img_line * size_pixel
 
+
 class Convert:
     def __init__(self, img):
         self.img = Image.open(img)
         self.bitmap = self.img.load()
-        self.imgC = Img_C()
+        self.imgC = self.toC()
 
     def toC(self):
+        img_c = Img_C()
         for i in range(size_pixel):
             for j in range(size_pixel):
-                for k in range(3):
+                for k in range(4):
                     self.imgC[i][j][k] = self.bitmap[i, j][k]
+        return img_c
 
     def toPython(self):
         self.bitmap = self.img.load()
@@ -33,7 +39,6 @@ class Convert:
         self.img.close()
 
     def invert_color(self):
-        self.toC()
         c_type.invert_color(self.imgC)
         self.toPython()
 
--- C/TP6/test.py
+++ C/TP6/test.py
@@ -1,22 +1,14 @@
-
-# ! /usr/bin/python3
+#! /usr/bin/python3
 
 from ctypes import *
+import timeit
 from PIL import Image
-
-# Fonction inversant les couleurs des pixels dans une image 512x512
-def invert_color_on_image(img):
-    bitmap = img.load()
-
-    for i in range(512):
-        for j in range(512):
-            (r, g, b, a) = bitmap[i, j]
-            bitmap[i, j] = (255 - r, 255 - g, 255 - b, a)
 
 
 img = Image.open("Lenna.png")
 
 
+# image_libc = cdll.LoadLibrary("/home/nborie/Enseignements/Marne_2018_2019/L3_C_avance/tp/tp_python_C/basic_image_process.so")
 image_libc = cdll.LoadLibrary("./image_process.so")
 print(image_libc.invert_color)
 
@@ -30,8 +22,7 @@ def convert_bitmap_to_c(img):
     bitmap = img.load()
     for i in range(512):
         for j in range(512):
-            for k in range(2):
-                img_c[i][j][k] = bitmap[i, j][k]
+            print(img_c[i][j])
     return img_c
 
 
@@ -43,7 +34,6 @@ def rebuild_image(img_c, img):
 
 
 img_c = convert_bitmap_to_c(img)
-
 
 
 img.show()
