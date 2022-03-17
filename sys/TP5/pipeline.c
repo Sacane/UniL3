054287906e21b0cc797e9e4c0ae236629002c688
Sacane <johan.ramaroson@gmail.com>
2022-03-17 20:08:11 +0100

    update

--- sys/TP5/pipeline.c
+++ sys/TP5/pipeline.c
@@ -18,23 +18,23 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    for(i = 0; i < argc; i++){
+
         
-        switch(try(fork())){
-            case 0:
-                try(close(p[0]));
-                dup2(p[1], STDOUT_FILENO);
-                execlp(argv[1], argv[1], (char*)NULL);
-                break;
-            default:
-                try(close(p[1]));
-                wait(NULL);
-                dup2(p[0], STDIN_FILENO);
-                execlp(argv[2], argv[2], (char*) NULL);
-                
-                break;
-        }
+    switch(try(fork())){
+        case 0:
+            try(close(p[0]));
+            dup2(p[1], STDOUT_FILENO);
+            execlp(argv[1], argv[1], (char*)NULL);
+            break;
+        default:
+            try(close(p[1]));
+            wait(NULL);
+            dup2(p[0], STDIN_FILENO);
+            execlp(argv[2], argv[2], (char*) NULL);
+            
+            break;
     }
+
 
 
 
