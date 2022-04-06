c03f1f79e6470f54b27d8ff39cf55176410a1685
Sacane <johan.ramaroson@gmail.com>
2022-04-06 09:42:32 +0200

    update tp8.a

--- sys/TP8a/main.c
+++ sys/TP8a/main.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "joblist.h"
 #include "try.h"
+
+
+#define CMD(job) job->pipeline->head->args->array[0]
 
 #define PROMPT "$ "
 
@@ -26,6 +31,15 @@ static char *readCmdLine(void) {
   return result;
 }
 
+int buildtinCD(int argc, char **argv){
+  if(argc < 2){
+    //TODO
+    return EXIT_SUCCESS;
+  } 
+  try(chdir(argv[1]));
+  return EXIT_SUCCESS;
+}
+
 int main(void) {
   while (true) {
     char *line = readCmdLine();
@@ -35,7 +49,23 @@ int main(void) {
     Job *job = newJobFromCmdLine(line);
     if (job) {
       // TODO: Do something more useful with the job!
-      printJob(job);
+      switch(try(job->id = fork())){
+        case 0:
+          if(!(strcmp("cd", CMD(job)))){
+            buildtinCD(job->pipeline->head->args->size, job->pipeline->head->args->array);
+          } else {
+            try(execvp(CMD(job), job->pipeline->head->args->array));
+          }
+          break;
+        default:
+          if(!job->bg){
+            try(wait(NULL));
+          }
+
+          break;
+      }
+
+
       delJob(job);
     }
   }
